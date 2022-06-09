d525dc93de18140790d2da21b43dc95b9d0f7862
Andrey Kudinov <2499931@gmail.com>
2022-06-09 13:21:07 +0400

    Add ts, eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  jsxBracketSameLine: true,
+  endOfLine: 'auto',
+  semi: false,
+  jsxSingleQuote: true
+}
